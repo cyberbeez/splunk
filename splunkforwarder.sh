e4c1a6c658875e0b07bc8b1133b693946c1df53f
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2025-02-02 20:10:15 -0500

    Update and rename splunkforwarder.bash to splunkforwarder.sh

---
+++ splunkforwarder.sh
@@ -0,0 +1,26 @@
+!/bin/bash
+
+#Written by Cameron Marquardt & Red Teamer John
+
+sudo wget "https://download.splunk.com/products/universalforwarder/releases/9.4.0/linux/splunkforwar$
+
+sudo mv ./splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz /opt/ 
+
+cd /opt
+
+sudo tar -xvzf splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz
+
+sudo /opt/splunkforwarder/bin/splunk start --accept-license
+
+#Shouldn't be there :( (Gathers computer IPv4 address, excludes loopback)
+#ip -4 addr show | grep -oP 'inet \K[\d.]+' | grep -v '^127\.0\.0\.1$'
+
+cd /opt/splunkforwarder/bin/
+
+sudo ./splunk enable boot-start
+
+sudo ./splunk add forward-server $1:8000
+
+sudo ./splunk add monitor /var/log/
+
+sudo ./splunk restart
