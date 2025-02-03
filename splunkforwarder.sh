22df42b432b91d629176c5cfb64d0475ce0abf97
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2025-02-02 20:11:04 -0500

    Update splunkforwarder.sh

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -2,12 +2,13 @@
 
 #Written by Cameron Marquardt & Red Teamer John
 
+##Grab File
 sudo wget "https://download.splunk.com/products/universalforwarder/releases/9.4.0/linux/splunkforwar$
-
+##Move File
 sudo mv ./splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz /opt/ 
 
 cd /opt
-
+##Unpack
 sudo tar -xvzf splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz
 
 sudo /opt/splunkforwarder/bin/splunk start --accept-license
