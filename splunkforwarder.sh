74a0a545cdb0f4f4af9be3c911187c16b5ac3433
ST3LTH-BKR <cmarqu04@baker.edu>
2025-02-04 21:32:55 -0500

    Update splunkforwarder.sh

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -15,10 +15,12 @@ sudo /opt/splunkforwarder/bin/splunk start --accept-license
 
 cd /opt/splunkforwarder/bin/
 ##Start on Boot
-sudo ./splunk enable boot-start
+sudo ./splunk enable boot-start -user sysadmin
+#Changes ownership recursively to prevent any ./splunk start issues in the future.
+sudo chmod -R sysadmin:sysadmin /opt/splunkforwarder
 ##Make sure to type home IP for splunk when running for the pos arg.
 sudo ./splunk add forward-server $1:8000
 ##Set Monitor
 sudo ./splunk add monitor /var/log/
 ##
-sudo ./splunk restart
+sudo ./splunk start
